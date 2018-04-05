40e4dae993b3579a1a420268b18c9d4646fab88b
andriamalala5056 <gastonandriamalala@gmail.com>
2018-04-05 18:02:50 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let container = document.querySelector('#content');
+let btn_view = document.querySelector("#afficherContainer");
+
+
+btn_view.addEventListener("click",function(){
+btn_view.style.display="none";
+container.style.display="inline-block";});
+
+
+//recherche
